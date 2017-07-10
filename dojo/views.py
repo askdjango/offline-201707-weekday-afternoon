86fd1fa4e0865de3be1782fb5316ea1f461ec3e3
Chinseok Lee <allieuslee@gmail.com>
2017-07-10 16:40:13 +0900

    한글매칭 URLConf 예제 추가

--- dojo/views.py
+++ dojo/views.py
@@ -6,3 +6,7 @@ def mysum(request, numbers):
     total = sum(int(number or 0) for number in numbers.split('/'))  # generator expression
     return HttpResponse(total)
 
+
+def hello(request, name, age):
+    return HttpResponse('안녕하세요. {}. {}살이시네요.'.format(name, age))
+
