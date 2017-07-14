3fd001756ddbb66e5f13c8623c1dc417c029aa92
Chinseok Lee <allieuslee@gmail.com>
2017-07-14 16:47:25 +0900

    blog.views.post_list 뷰에서 검색 구현

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,13 @@ from .models import Post
 
 def post_list(request):
     qs = Post.objects.all()
+
+    query = request.GET.get('query', '')
+    if query:
+        qs = qs.filter(title__icontains=query)
+
     return render(request, 'blog/post_list.html', {
         'post_list': qs,
+        'query': query,
     })
 
