1e4d246c319f3d2d40c88141f8f431f91dfbd734
Chinseok Lee <allieuslee@gmail.com>
2017-07-19 14:35:59 +0900

    blog앱에 url reverse 적용

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^weblog/', include('blog.urls', namespace='blog')),
     url(r'^dojo/', include('dojo.urls')),
 ]
 
