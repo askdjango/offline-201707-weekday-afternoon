cbd1b3f4b92ecb7eda2735272c7493d81e5bfcde
Chinseok Lee <allieuslee@gmail.com>
2017-07-19 16:10:24 +0900

    blog.models.Post 모델에 help_text/validators 적용

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,16 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
 
 
+def min_length_3(value):
+    if len(value) < 3:
+        raise ValidationError('3글자 이상 입력해주세요.')
+
+
 class Post(models.Model):
-    author = models.CharField(max_length=20)
-    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=20, help_text='포스팅 작성자 이름을 입력해주세요.')
+    title = models.CharField(max_length=100, validators=[min_length_3])
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
