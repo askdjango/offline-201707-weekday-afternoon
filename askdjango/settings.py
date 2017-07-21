5a9b09eb8660d2c5748b26f10dbc8811392de76a
Chinseok Lee <allieuslee@gmail.com>
2017-07-21 15:13:49 +0900

    settings에 MEDIA 설정

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -125,5 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 INTERNAL_IPS = ['127.0.0.1']
 
