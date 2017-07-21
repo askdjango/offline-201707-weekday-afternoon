efe4ac06ba17d3ce42e8d5aaec19e47a8c9d8551
Chinseok Lee <allieuslee@gmail.com>
2017-07-21 16:35:32 +0900

    랜덤 upload_to 함수 구현

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,5 @@
+import os
+import uuid
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.urls import reverse
@@ -8,11 +10,19 @@ def min_length_3(value):
         raise ValidationError('3글자 이상 입력해주세요.')
 
 
+def random_upload_to(instance, orig_filename):
+    # 파일명을 포함한 모든 경로를 기대
+    ext = os.path.splitext(orig_filename)[-1].lower()
+    filename = uuid.uuid4().hex + ext
+    filepath = os.path.join(filename[:3], filename[3:6], filename[6:9], filename[9:])
+    return filepath
+
+
 class Post(models.Model):
     author = models.CharField(max_length=20, help_text='포스팅 작성자 이름을 입력해주세요.')
     title = models.CharField(max_length=100, validators=[min_length_3])
     content = models.TextField()
-    photo = models.ImageField(upload_to='blog/post/%Y/%m/%d')
+    photo = models.ImageField(upload_to=random_upload_to)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
