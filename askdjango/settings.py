e9b40aa61c90f90d9764fcc0117da72b97a73d75
Chinseok Lee <allieuslee@gmail.com>
2017-07-21 14:57:13 +0900

    settings.STATIC_ROOT 설정 및 static 샘플

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 INTERNAL_IPS = ['127.0.0.1']
 
