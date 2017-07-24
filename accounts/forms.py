6cf217fffa9fc98b3014eb0fe2ed2354614aa8cb
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 16:18:36 +0900

    모델폼 커스텀 help_text 예시 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,3 +5,8 @@ class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ('email',)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = '내가 원하는 도움말'
+        self.fields['email'].help_text = '내가 원하는 Email 도움말'
+
