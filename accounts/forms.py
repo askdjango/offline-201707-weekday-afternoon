96d7e5c3dde73c2e695df3852b6a89dd652ece45
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 16:24:43 +0900

    로그인 커스텀 구현

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class SignupForm(UserCreationForm):
@@ -9,4 +10,12 @@ class SignupForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].help_text = '내가 원하는 도움말'
         self.fields['email'].help_text = '내가 원하는 Email 도움말'
+
+
+def check_answer(value):
+    if value != 6:
+        raise forms.ValidationError('땡~!')
+
+class LoginForm(AuthenticationForm):
+    answer = forms.IntegerField(help_text='3+3=?', validators=[check_answer])
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from .forms import LoginForm
 from . import views
 
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout', kwargs={
+    url(r'^login/$', auth_views.login, name='login', kwargs={
+        'form_class': LoginForm,
+    }),
+    url(r'^logout/$', auth_views.logout, name='logout', kwargs={
         'next_page': settings.LOGIN_URL,  # LogoutView CBV에서는 현재 처리되고 있지 않음.
     }),
     url(r'^profile/$', views.profile, name='profile'),
