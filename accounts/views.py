15e3789b890581aef8948492c3081bf4f62adf5c
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 15:18:39 +0900

    accounts.views.profile 뷰에 login_required 장식자 적용

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def profile(request):
-    # request.user
     return render(request, 'accounts/profile.html')
 
