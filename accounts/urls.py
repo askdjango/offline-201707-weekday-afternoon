807156a960be1ecfd64808aa40eec446467ff470
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 15:23:11 +0900

    회원 가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^profile/$', views.profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect(settings.LOGIN_URL)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup_form.html', {
+        'form': form,
+    })
 
 
 @login_required
