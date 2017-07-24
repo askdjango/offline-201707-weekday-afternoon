e81234f3fc66b25546df17e707db436c9f05625d
Chinseok Lee <allieuslee@gmail.com>
2017-07-24 15:16:10 +0900

    프로필 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def profile(request):
+    # request.user
+    return render(request, 'accounts/profile.html')
+
